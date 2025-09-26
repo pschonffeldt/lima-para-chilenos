7bf455ad970a746daaf6ca6f8ccb60e6450615d8
Pablo S <cineschonffeldt@gmail.com>
2025-09-26 16:50:07 -0500

    Add extra routing and pages

---
+++ src/app/dinero/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function DineroPage() {
+  return <div>DineroPage</div>;
+}
---
+++ src/seguridad/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function SeguridadPage() {
+  return <div>SeguridadPage</div>;
+}
